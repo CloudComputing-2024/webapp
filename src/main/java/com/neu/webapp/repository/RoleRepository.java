89ab33aaa4759ae51c9102901069df25f6bfcd8e
Jiapan Wei <wei.jiap@northeastern.edu>
2024-02-07 17:10:38 -0800

    Modify UserRepository and RoleRepository

--- src/main/java/com/neu/webapp/repository/RoleRepository.java
+++ src/main/java/com/neu/webapp/repository/RoleRepository.java
@@ -2,9 +2,11 @@ package com.neu.webapp.repository;
 
 import com.neu.webapp.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
     Optional<Role> findByName(String roleName);
 }
--- src/main/java/com/neu/webapp/repository/UserRepository.java
+++ src/main/java/com/neu/webapp/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.neu.webapp.repository;
 
 import com.neu.webapp.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     // find userEntity by username
     Optional<UserEntity> findByUsername(String username);
