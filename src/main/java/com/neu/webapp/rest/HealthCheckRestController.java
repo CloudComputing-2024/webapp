f8bde1d20e7ffa8d41db147ccc5a79cc11309258
Jiapan Wei <wei.jiap@northeastern.edu>
2024-02-08 18:04:51 -0800

    Update healthz API

--- src/main/java/com/neu/webapp/rest/HealthCheckRestController.java
+++ src/main/java/com/neu/webapp/rest/HealthCheckRestController.java
@@ -28,19 +28,7 @@ public class HealthCheckRestController {
 
             // check for query parameters
             if (!request.getParameterMap().isEmpty()) {
-                return ResponseEntity.badRequest()
-                                     .header("Access-Control-Allow-Credentials", "true")
-                                     .header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,Accept,Origin")
-                                     .header("Access-Control-Allow-Methods", "*")
-                                     .header("Access-Control-Allow-Origin", "*")
-                                     .header("Cache-Control", "no-cache")
-                                     .header("Content-Type", "application/octet-stream")
-                                     .header("Expires", "-1")
-                                     .header("X-Powered-By", "Express")
-                                     .header("Pragma", "no-cache")
-                                     .header("X-Content-Type-Options", "nosniff")
-                                     .header("Allow", "GET")
-                                     .build();
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             }
 
             // check for request payload
@@ -49,19 +37,8 @@ public class HealthCheckRestController {
             }
 
             // respond with HTTP 200 OK status if database connection is successful
-            return ResponseEntity.ok()
-                                 .header("Access-Control-Allow-Credentials", "true")
-                                 .header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,Accept,Origin")
-                                 .header("Access-Control-Allow-Methods", "*")
-                                 .header("Access-Control-Allow-Origin", "*")
-                                 .header("Cache-Control", "no-cache")
-                                 .header("Content-Type", "application/octet-stream")
-                                 .header("Expires", "-1")
-                                 .header("X-Powered-By", "Express")
-                                 .header("Pragma", "no-cache")
-                                 .header("X-Content-Type-Options", "nosniff")
-                                 .header("Allow", "GET")
-                                 .build();
+            return new ResponseEntity<>(HttpStatus.OK);
+
         } catch (PersistenceException exception) {
             // throw to SQLException if database connection is unsuccessful
             throw new SQLException("Invalid Database Connection");
@@ -71,36 +48,12 @@ public class HealthCheckRestController {
     // Not allowed HEAD request for "/healthz"
     @RequestMapping(value = "/healthz", method = RequestMethod.HEAD)
     public ResponseEntity<String> handleHeadForHealthz() {
-        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
-                             .header("Access-Control-Allow-Credentials", "true")
-                             .header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,Accept,Origin")
-                             .header("Access-Control-Allow-Methods", "*")
-                             .header("Access-Control-Allow-Origin", "*")
-                             .header("Cache-Control", "no-cache")
-                             .header("Content-Type", "application/octet-stream")
-                             .header("Expires", "-1")
-                             .header("X-Powered-By", "Express")
-                             .header("Pragma", "no-cache")
-                             .header("X-Content-Type-Options", "nosniff")
-                             .header("Allow", "GET")
-                             .build();
+        return new ResponseEntity<>(HttpStatus.METHOD_NOT_ALLOWED);
     }
 
     // Not allowed OPTIONS request for "/healthz"
     @RequestMapping(value = "/healthz", method = RequestMethod.OPTIONS)
     public ResponseEntity<String> handleOptionsForHealthz() {
-        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
-                             .header("Access-Control-Allow-Credentials", "true")
-                             .header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,Accept,Origin")
-                             .header("Access-Control-Allow-Methods", "*")
-                             .header("Access-Control-Allow-Origin", "*")
-                             .header("Cache-Control", "no-cache")
-                             .header("Content-Type", "application/octet-stream")
-                             .header("Expires", "-1")
-                             .header("X-Powered-By", "Express")
-                             .header("Pragma", "no-cache")
-                             .header("X-Content-Type-Options", "nosniff")
-                             .header("Allow", "GET")
-                             .build();
+        return new ResponseEntity<>(HttpStatus.METHOD_NOT_ALLOWED);
     }
 }
