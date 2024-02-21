9a874df6a9e32d1b01ac5232eec83b190c0bf096
jiapanWei <113729007+jiapanWei@users.noreply.github.com>
2024-02-20 23:26:02 -0800

    Update workflow (#60)

--- packer/scripts/systemd.sh
+++ packer/scripts/systemd.sh
@@ -1,3 +1,3 @@
-sudo mv /opt/webapp.service /etc/systemd/system/webapp.service
+#sudo mv /opt/webapp.service /etc/systemd/system/webapp.service
 sudo systemctl daemon-reload
 sudo enable webapp.service
