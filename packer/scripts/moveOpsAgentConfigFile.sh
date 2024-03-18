c3502b33fa2c5501c291a36e5b9c8992dabe3be8
jiapanWei <113729007+jiapanWei@users.noreply.github.com>
2024-03-18 14:43:22 -0700

    Install ops agent and build image (#116)

--- packer/scripts/moveOpsAgentConfigFile.sh
+++ packer/scripts/moveOpsAgentConfigFile.sh
@@ -1,4 +1,3 @@
 sudo mv /tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml
 sudo chown root:root /etc/google-cloud-ops-agent/config.yaml
 sudo chmod 755 /etc/google-cloud-ops-agent/config.yaml
-sudo systemctl restart google-cloud-ops-agent
